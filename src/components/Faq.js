6aef3571d968e51e3fce47f1ed6f332f25e51e0a
Andrea <Andrea@Samuel-Reed.local>
2017-03-14 19:26:45 -0400

    Update copy

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -48,12 +48,13 @@ const Faq = () => {
         <p>We met in March 2015 when Lincoln (going to school in Arizona) was visiting his friend Victoria in NYC.
         Victoria introduced him to her best friend, Andrea (living in Brooklyn) and we became friends right away. At the end of the trip, we acknowledged that we might never see each other again, but agreed to become pen pals.</p>
       <p>After six months of writing letters, we started flying across the country to visit each other, and it
-          wasn't long before we were dating.🙋🏻‍♂️🙋🏽👉🏽💌👉🏽✈️👉🏽💑👉🏽💍</p>
+          wasn't long before we were dating.</p>
+
+        <p> Here's a diagram: 🙋🏻‍♂️ 🙋🏻 👉  💌 👉  ✈️ 👉  💑 👉 💍</p>
         <br />
         <p><strong>how did you get engaged?</strong></p>
         <p>We got engaged just after Christmas on the way from Lincoln's parents house in Mesa, AZ to Andrea's parents in
-          St. George, UT. We stopped at Horseshoe Bend, a beautiful section of the Colorado River. It was pretty crowded,
-          but Lincon was undeterred. He insisted on walking around the edge of the cliff until we found a secluded spot where he could get down on one knee.</p>
+          St. George, UT. We stopped at Horseshoe Bend, a beautiful section of the Colorado River. It was pretty crowded-- nevertheless, he persisted. Lincoln insisted on walking around the edge of the cliff until we found a secluded spot where he could get down on one knee.</p>
         <p>The photo above was taken minutes after Lincoln proposed. Please notice the majestic view.</p>
         <p><strong>where will you live?</strong></p>
         <p>We're moving to Boston! Lincoln's short stint in NYC has come to an end, as he has just accepted a job and will be moving right before the wedding. Andrea is leaving her position on the Anthropologie visual team and hopes to become a professional knitter.</p>
--- src/components/TravelInfo.js
+++ src/components/TravelInfo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styles from '../../styles/application.scss';
 
 const localStyles = {
   container: {
@@ -12,13 +13,17 @@ const localStyles = {
   },
   imgContainer: {
     "marginTop": "3%",
-    "marginBottom": "3%"
+    "marginBottom": "3%",
+    "width": "100%"
   },
   img: {
     "maxWidth": "100%",
+    "width": "100%",
     "display":"block",
     "margin":"auto",
     "padding":"1%",
+    "border":"1px solid",
+    "borderColor":styles.borderColor
   },
   msgContainer: {
     "width": "100%",
@@ -43,18 +48,17 @@ const Registry = () => {
         <p>If you are traveling to see us, THANK YOU!! It truly means alot and we are sure you'll love your stay.</p>
         <p>The easiest airport to fly into is <strong>Phoenix Sky Harbor</strong> but you can also fly into the <strong>Phoenix-Mesa Gateway Airport.</strong></p>
         <br />
-        <p><strong>venue:</strong></p>
-        <p>Schoenburg Gardens<br />
-        4210 E Houston Ave<br />
-        Gilbert, AZ 85234</p>
+        <p>As you make plans, you may want to keep these events in mind:</p>
+        <p><strong>Friday 6:00pm</strong> Family Barbeque at <a href="https://goo.gl/maps/ivQQWU3qHyp" target="_blank">Freestone Park</a></p>
+        <p><strong>Saturday 8:00am</strong> Kid's Easter Egg Hunt at <a href="https://goo.gl/maps/v7PF9kbCKjE2" target="_blank">Harmony Park</a></p>
+        <p><strong>Saturday 2:30pm</strong> Pictures at <a href="https://goo.gl/maps/UpyrBCsvkin" target="_blank">Mesa Temple</a></p>
+        <p><strong>Saturday 6:00pm</strong> Reception at <a href="https://goo.gl/maps/VmrUEjm7bZw" target="_blank">Schoenburg Gardens</a></p>
         <p>Parking will be available...</p>
-        <p>(map coming soon)</p>
-        <br />
+        <br /><br />
         <p><strong>While you're in the area, we recommend checking out:</strong></p>
         <p>The <a href="http://easterpageant.org/" target="_blank">Mesa Temple Easter Pageant</a> will be happening Tuesday-Friday evening.<br /></p>
         <p>Southern food in a 1940's Valentine Diner at <a href="http://welcomediner.net/garfield" target="_blank"> Welcome Diner.</a></p>
       </div>
-
     </div>
 
   );
